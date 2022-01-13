cbe820f3eba1cb318ea678580143e1832cb69a4d
Diego Díaz <didimar2001@gmail.com>
2022-01-12 21:00:32 -0500

    Projects section

---
+++ resources/resources.js
@@ -0,0 +1,34 @@
+export const resources = [
+  {
+    id: 1,
+    imgDir: require("public/assets/todo_app.jpeg"),
+    title: "#ToDo List",
+    description:
+      "This application/site was created as a submission to a DevChallenges challenge. The challenge was to build an application to complete the given user stories.",
+    link: "https://apophthegm.github.io/to-do-list/",
+  },
+  {
+    id: 2,
+    imgDir: require("public/assets/404_error.jpeg"),
+    title: "404 Error Page",
+    description:
+      "A traditional 404 error page with an animation. This project have responsive design and it's adaptable to all devices.",
+    link: "https://apophthegm.github.io/page-404/",
+  },
+  {
+    id: 3,
+    imgDir: require("public/assets/weather-app.jpeg"),
+    title: "Weather Desktop App",
+    description:
+      "It's a weather app based on desktop devices. This projects was done with React and Sass.",
+    link: "https://apophthegm.github.io/weather-react-app/",
+  },
+  {
+    id: 4,
+    imgDir: require("public/assets/estuarg.jpeg"),
+    title: "EstuArg.com",
+    description:
+      "This is the web page of a travel consulting agency for studying in Argentina.",
+    link: "https://yelenka.vercel.app",
+  },
+];
